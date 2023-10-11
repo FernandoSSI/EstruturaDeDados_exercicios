b8715397a93e30f7e7503fb6f0655d564676d52c
FernandoSSI <fernandosi2005@gmail.com>
2023-10-11 20:39:44 -0300

    Update qst2, versão final

--- exercício_pratico6/qst2.c
+++ exercício_pratico6/qst2.c
@@ -1,61 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
-// nó de operadores
+//nó de valores
 typedef struct no{
-    char digit;
+    float valor;
     struct no * prox;
     struct no * ant;
-} NO;
+}NO;
 
-// nó de resultados
-typedef struct no_result{
-    float valor;
-    struct no_result * prox;
-    struct no_result * ant;
-} NO_RESULT;
+NO * final = NULL;
+NO * inicio = NULL;
 
-NO * topo = NULL;
-NO_RESULT * topo_result= NULL;
-NO * fim = NULL;
-NO_RESULT * fim_result= NULL;
-
-// função para empilhar os operadores
-void empilharOp(char digit){
+// func para add na fila
+void add(float valor){
     NO * novo = malloc(sizeof(NO));
-    novo -> digit = digit;
-    novo -> prox = NULL;
-    novo -> ant = NULL;
-    if(!fim){
-        topo = novo;
-        fim = novo;
-    } else {
-        fim -> prox = novo;
-        novo -> ant = fim;
-        fim = novo;
-        
-    }
-}
-
-//função para empilhar numeros
-void empilharResult(float valor){
-    NO_RESULT * novo = malloc(sizeof(NO_RESULT));
     novo -> valor = valor;
-    novo -> prox = NULL;
-    novo -> ant = NULL;
-    if(!fim_result){
-        topo_result = novo;
-        fim_result = novo;
+    if(!final){
+        final = novo;
+        inicio = novo;
     } else {
-        fim_result -> prox = novo;
-        novo -> ant = fim_result;
-        fim_result = novo;
+        final -> prox = novo;
+        novo -> ant = final;
+        final= novo;
     }
 }
 
-// função para calcular baseado na operação
+//func para remover no fim da fila
+void remover(){
+    if(final){
+        NO * aux = final;
+        final = aux->ant;
+        
+        free(aux);
+    }else{
+        printf("A lista está vazia, não consigo remover");
+    }
+}
+
+// func de operações
 float operacao (float a, float b, char operador){
     switch(operador){
         case '+':
@@ -75,45 +58,54 @@ float operacao (float a, float b, char operador){
     }
 }
 
-// função para calcular e imprimir
-void calcular(char exp[]){
-    for (int i =0; exp[i] != '\0'; i++){
-        if(!isdigit(exp[i])){
-            empilharOp(exp[i]);
-        } else {
-            empilharResult(exp[i]-'0');
+//func para calcular
+void calc(char palavra[]){
+    char p1[100];
+    int tam = strlen(palavra);
+    char *pt;
+    
+    for(int i=0; i<tam; i++){
+        p1[i] = palavra[i];
+    }
+    p1[tam] = '\0';
+    
+    
+    pt = strtok(p1, " ");
+    while(pt){
+        printf("%s ", pt);
+        float num;
+        
+        if(pt[0] >= '0' && pt[0] <= '9'){
+            num = atof(pt);
+            add(num);
+        } else{
+            
+            float result=operacao(final->ant->valor, final->valor,pt[0]);
+            
+            if(final){
+                remover();
+                if(final){
+                    remover();
+                    add(result);
+                }
+                
+            }
         }
+        pt = strtok(NULL, " ");
     }
+   
+    printf(" = %.2f \n", inicio-> valor);
     
-    float a, b;
-    char op;
-    float result;
-    NO * auxop = topo;
-
-    while(auxop){
-        NO_RESULT * aux = topo_result;
-        a = aux -> valor;
-        b = aux -> prox -> valor;
-        op = auxop -> digit;
-        result = operacao(a, b, op);
-        
-        empilharResult(result);
-        
-        topo_result = aux -> prox -> prox;
-        topo_result -> ant = NULL;
-        aux->prox -> prox = NULL;
-        aux->prox -> ant = NULL;
-        free(aux->prox);
-        free(aux);
-        
-        auxop = auxop -> prox;
-    }
-    
-    printf("%s: %.2f\n",exp, topo_result-> valor);  
+    free(inicio);
+    inicio = NULL;
+    final = NULL;
 }
 
-int main() {
-    
-    calcular("12-45+*");
+int main(){
+
+    calc("1 2 - 4 5 + *");
+    calc("12 6 - 10 1 + *");
+    calc("15 6 * 6 /");
+
     return 0;
 }
